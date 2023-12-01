6eae1c3816479fe386313d2f1fef385e0da5a23e
Michael Mayer <mayermichael79@gmail.com>
2023-12-01 19:02:12 +0100

    better GAM, better random forest in readme

---
+++ backlog/plot_settings.R
@@ -0,0 +1,5 @@
+ggsave("man/figures/README-rf-imp.svg", scale = 2)
+ggsave("man/figures/README-rf-dep.svg", width = 8.5, height = 6)
+
+ggsave("man/figures/README-gam-imp.svg", scale = 2)
+ggsave("man/figures/README-gam-dep.svg", width = 8.5, height = 6)
