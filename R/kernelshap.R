74be45d5f3b5f664d370e20022abebe31c90ddb2
Michael Mayer <mayermichael79@gmail.com>
2024-08-03 16:05:35 +0200

    fix problematic argument survival in ranger models

--- R/kernelshap.R
+++ R/kernelshap.R
@@ -359,12 +359,11 @@ kernelshap.ranger <- function(
     survival = c("chf", "prob"),
     ...
   ) {
-  survival <- match.arg(survival)
-  
+
   if (is.null(pred_fun)) {
-    pred_fun <- pred_ranger
+    pred_fun <- create_ranger_pred_fun(object$treetype, survival = match.arg(survival))
   }
-  
+
   kernelshap.default(
     object = object, 
     X = X,
@@ -381,7 +380,6 @@ kernelshap.ranger <- function(
     parallel = parallel,
     parallel_args = parallel_args,
     verbose = verbose,
-    survival = survival,
     ...
   )
 }
